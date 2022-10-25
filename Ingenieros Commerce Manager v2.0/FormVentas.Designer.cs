2001715af7ee9e6ed84de9889d0e00a43e5dbaef
santi-esquerre <bymaxxeryt@gmail.com>
2022-10-25 09:20:10 -0300

    little changes

--- Ingenieros Commerce Manager v2.0/FormVentas.Designer.cs
+++ Ingenieros Commerce Manager v2.0/FormVentas.Designer.cs
@@ -35,7 +35,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(799, 458);
+            this.ClientSize = new System.Drawing.Size(770, 610);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FormVentas";
             this.Text = "FormVentas";
